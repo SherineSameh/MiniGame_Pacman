83e3dc4977fa4adc8b31de87dbb787f665ccf6b8
myriamreda9 <myriam.dimitry94@gmail.com>
2016-04-26 02:57:40 +0200

    Database

--- index.php
+++ index.php
@@ -15,8 +15,43 @@
 </head>
 
 <body>
-	
+	<form method='post' action='index.php'>
+<!-- i prefer tezabatyh    -->
 	<script type="text/javascript" src="js/phaser.js"></script>
 	<script type="text/javascript" src="js/splash.js"></script>
 </body>
 </html>
+<?php
+mysql_connect("localhost","root","")or die(mysql_error());
+mysql_select_db("Pacman");
+
+if(isset($_POST['New_Player'])){
+    $name =$_POST['PName'];
+  $sqll = "INSERT INTO `Pacman`.`Player` ( `PName`) VALUES ( '$name')";
+if(mysql_query($sqll)){
+echo "<script>alert(' registration submission accomplished ')</script>";
+
+}}
+elseif(isset($_POST['login'])){
+    $user =$_POST['userl'];
+ $query = mysql_query("SELECT * FROM Player where Pname ='$_POST[userl]' ") or die(mysql_error());
+}
+
+
+/*$servername="localhost";
+$sqluser="root";
+$sqlpass="";
+$db="Pacman";
+$connect =mysqli_connect($servername,$sqluser,$sqlpass);
+if(!$connect)
+{
+die("Connection Failed.<br>".mysqli_error());
+}
+
+$select=mysqli_select_db($connect,$db);
+if(!$select)
+{
+	die("Couldn't select db.<br>".mysqli_error($connect));
+}
+*/
+?>
