768fe604f31c52b6b005b1ec1dade5fe9979871e
sherine sameh <sherine-sameh@hotmail.com>
2016-04-29 13:29:58 +0200

    organize code

--- js/splash.js
+++ js/splash.js
@@ -1,37 +1,23 @@
-var
-  game = new Phaser.Game(780, 600, Phaser.AUTO, 'game'),
-  splash = function () {},
-  gameOptions = {
-    playSound: true,
-    playMusic: true
-  },
-  music,musicState;
+splash = function () {},
 
 splash.prototype = {
 
   preload: function () {
 
-    game.load.image('bg','assets/images/bg.png');
     game.load.image('menuBg','assets/images/menuBg.png');
     game.load.image('newGame','assets/images/newGame_.png');
     game.load.image('options','assets/images/options_.png');
     game.load.image('highScore','assets/images/highScore_.png');
-    
+    game.load.image('score_label','assets/images/score_label.png');
+    game.load.image('back_icon','assets/images/back_icon.png');
+    game.load.image('Life','assets/images/life.png');
     game.load.image('gameOver','assets/images/gameOver.png');
     game.load.image('win','assets/images/Win.png');
-
     game.load.image('back','assets/images/back.png');
+    
     game.load.spritesheet('play_mute', 'assets/images/play_mute.png', 385, 45);
-
-    game.load.audio('intro','assets/sounds/intro.wav');
-
-    game.load.script('game',  'js/game.js');
-    game.load.script('menu',  'js/menu.js');
-    game.load.script('options',  'js/options.js');
-    game.load.script('score',  'js/score.js');
-    game.load.script('GameOver',  'js/GameOver.js');
-    game.load.script('win',  'js/win.js');
-
+    game.load.spritesheet('play_mute_icon', 'assets/images/play_mute_icon_.png', 40, 40);
+    game.load.spritesheet('pause_play_icon', 'assets/images/pause_play_icon.png', 40, 40);
 
     game.load.spritesheet('pacman', 'assets/images/pacman.png', 30, 30);
     game.load.spritesheet('pinky', 'assets/images/pinky.png', 30, 30);
@@ -40,18 +26,20 @@ splash.prototype = {
     game.load.spritesheet('clyde', 'assets/images/clyde.png', 30, 30);
     game.load.spritesheet('dead_pacman', 'assets/images/dead_pacman.png', 30, 30);
 
-    game.load.image('white_bg','assets/images/white_bg.png');
-    game.load.image('score_label','assets/images/score_label.png');
-    game.load.image('back_icon','assets/images/back_icon.png');
-    game.load.image('Life','assets/images/life.png');
-    game.load.spritesheet('play_mute_icon', 'assets/images/play_mute_icon_.png', 40, 40);
-    game.load.spritesheet('pause_play_icon', 'assets/images/pause_play_icon.png', 40, 40);
+    game.load.script('game',  'js/game.js');
+    game.load.script('menu',  'js/menu.js');
+    game.load.script('options',  'js/options.js');
+    game.load.script('score',  'js/score.js');
+    game.load.script('GameOver',  'js/GameOver.js');
+    game.load.script('win',  'js/win.js');
 
     game.load.tilemap('myTilemap','assets/tilemaps/PacmanMap.json',  null, Phaser.Tilemap.TILED_JSON);
     game.load.image('Tile','assets/tilemaps/tile.png');
     game.load.image('Dot','assets/tilemaps/dot.png')
     game.load.image('Bg','assets/tilemaps/bg.png')
    
+
+    game.load.audio('intro','assets/sounds/intro.wav');
     game.load.audio('doteat', 'assets/sounds/doteat.mp3');    
     game.load.audio('pacend', 'assets/sounds/pacend.mp3');
     game.load.audio('endlevel', 'assets/sounds/endlevel.mp3'); 
@@ -59,6 +47,9 @@ splash.prototype = {
     
   },
   
+  init: function () {
+    game.add.sprite(0, 0, 'bg');
+  },
   addGameStates: function () {
 
     game.state.add("Game",Game);
@@ -71,8 +62,7 @@ splash.prototype = {
       },
 
   create: function () {
-    game.add.sprite(0, 0, 'bg');
-
+    
     music = game.add.audio('intro');
     music.loop = true;
     music.play();
@@ -85,5 +75,3 @@ splash.prototype = {
   }
  };
 
-game.state.add("splash", splash);
-game.state.start('splash');
